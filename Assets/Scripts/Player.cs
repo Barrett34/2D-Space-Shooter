47e67ad9b0af2d63c312dbd41c5c0821cb28ba9d
Barrett Morrissette <bmorrissette34@gmail.com>
2022-10-30 17:15:34 -0500

    added player bounds for x and y axis

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,17 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // public or private reference
-    // data type (int, float, bool, string)
-    // every variable has a name
-    // optional value assigned
 
     [SerializeField]
     private float _speed = 3.5f;
-
-
-    // SerializeField allows you to access private variables on the unity editor
-
 
     // Start is called before the first frame update
     void Start()
@@ -26,16 +18,18 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        transform.Translate(Vector3.right * horizontalInput * _speed *  Time.deltaTime);
-        transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
 
-        // if player position on the y is greater than 0
-        // y position = 0
-        // else if position on the y is -3.8f
-        // y position = -3.8f
+        transform.Translate(direction * _speed * Time.deltaTime);
+        
 
         if (transform.position.y >= 0)
         {
@@ -46,10 +40,13 @@ public class Player : MonoBehaviour
             transform.position = new Vector3(transform.position.x, -3.8f, 0);
         }
 
-        // if player on the x > 11
-        // x pos = -11
-        // else if player on the x is less than -11
-        // x pos = 11
-
+        if (transform.position.x >= 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
     }
 }
