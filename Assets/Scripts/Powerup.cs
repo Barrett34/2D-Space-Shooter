e4f31a4846bcfd723dd9c52c2b40cd99321da28e
Barrett Morrissette <bmorrissette34@gmail.com>
2022-12-12 18:19:59 -0600

    added speed powerup and functionality

--- Assets/Scripts/Powerup.cs
+++ Assets/Scripts/Powerup.cs
@@ -7,6 +7,12 @@ public class Powerup : MonoBehaviour
 
     [SerializeField]
     private float _speed = 3f;
+    //create id for power up
+    // 0 = triple shot
+    // 1 = speed
+    // 2 = shields
+    [SerializeField]
+    private int _powerupID;
 
     void Start()
     {
@@ -32,8 +38,20 @@ public class Powerup : MonoBehaviour
             Player player = other.transform.GetComponent<Player>();
             if (player != null)
             {
-                player.TripleShotActive();
-                
+                switch(_powerupID)
+                {
+                    case 0:
+                        player.TripleShotActive();
+                        break;
+                    case 1:
+                        Debug.Log("Collected Speed Boost");
+                        break;
+                    case 2:
+                        Debug.Log("Shield activated");
+                        break;
+                    default:
+                        break;
+                }
             }
 
             Destroy(this.gameObject);
