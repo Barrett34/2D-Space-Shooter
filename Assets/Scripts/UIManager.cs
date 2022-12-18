42237a5487e166415c3d897ba8fa5d4b6428b23a
Barrett Morrissette <bmorrissette34@gmail.com>
2022-12-18 17:45:30 -0600

    added Game Over text and flicker when player runs out of lives

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -12,6 +12,8 @@ public class UIManager : MonoBehaviour
     private Sprite[] _liveSprites;
     [SerializeField]
     private Image _LivesImg;
+    [SerializeField]
+    private Text _gameOver;
 
 
     // Start is called before the first frame update
@@ -19,6 +21,7 @@ public class UIManager : MonoBehaviour
     {
 
         _scoreText.text = "Score: " + 0;
+        _gameOver.gameObject.SetActive(false);
     }
 
     public void UpdateScore(int playerScore)
@@ -28,9 +31,23 @@ public class UIManager : MonoBehaviour
 
     public void UpdateLives(int currentLives)
     {
-        // display img sprite
-        // give it a new one base on current lives index
 
         _LivesImg.sprite = _liveSprites[currentLives];
+
+        if(currentLives == 0)
+        {
+            StartCoroutine(GameOverFlickerRoutine());
+        }
+    }
+
+    IEnumerator GameOverFlickerRoutine()
+    {
+        while (true)
+        {
+            _gameOver.gameObject.SetActive(true);
+            yield return new WaitForSeconds(0.5f);
+            _gameOver.gameObject.SetActive(false);
+            yield return new WaitForSeconds(0.5f);
+        }
     }
 }
