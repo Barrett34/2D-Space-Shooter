151981d15e8a1ff6375466f2d4e41a1be040d423
Barrett Morrissette <bmorrissette34@gmail.com>
2022-12-18 15:08:07 -0600

    added lives tracking ui system

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -8,16 +8,29 @@ public class UIManager : MonoBehaviour
     //handle to text
     [SerializeField]
     private Text _scoreText;
+    [SerializeField]
+    private Sprite[] _liveSprites;
+    [SerializeField]
+    private Image _LivesImg;
 
 
     // Start is called before the first frame update
     void Start()
     {
+
         _scoreText.text = "Score: " + 0;
     }
 
     public void UpdateScore(int playerScore)
     {
         _scoreText.text = "Score: " + playerScore;
+    }
+
+    public void UpdateLives(int currentLives)
+    {
+        // display img sprite
+        // give it a new one base on current lives index
+
+        _LivesImg.sprite = _liveSprites[currentLives];
     }
 }
